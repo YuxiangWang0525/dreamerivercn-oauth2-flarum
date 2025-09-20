f33bdeb62eb3a9acfb5700054edac56f3726e620
YuxiangWang_0525(John Frederick Wang) <66881174+YuxiangWang0525@users.noreply.github.com>
2025-09-20 23:50:41 +0800

    add adminjs

--- js/dist/admin.js
+++ js/dist/admin.js
@@ -1,1 +1,8 @@
-﻿
+(function() {
+  'use strict';
+
+  app.initializers.add('dreamerivercn/oauth2-flarum', function(app) {
+    // 插件已成功加载
+    console.log('Dreameriver.cn OAuth2 Flarum插件管理端已加载');
+  });
+})();
